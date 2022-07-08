8e4f4d7d0a4a1262155c7a150b52865256311528
tpronicheva <tpronicheva@smartlabs.tv>
2022-07-08 19:42:06 +0300

    Initial commit

---
+++ src/test/java/ru/gb/demoSecurity/DemoSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.demoSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
